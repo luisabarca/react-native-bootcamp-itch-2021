896b91e79a4d6b7c59e246334b86312ee24fdccc
Luis Abarca <luisabarca@gmail.com>
2021-08-25 18:06:48 -0500

    Agregamos la pantalla para el inicio de sesión y la lógica

---
+++ src/pantallas/Login/index.js
@@ -0,0 +1,134 @@
+import React, { useRef, useState, useEffect } from "react";
+import { View, Button } from "react-native";
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
+import firebase from 'firebase';
+import "firebase/auth";
+
+import InputPhoneItch from "../../componentes/InputPhoneItch";
+import LogoItch from "../../componentes/Logo";
+import { firebaseConfig } from '../../utils';
+import styles from './Login.styles';
+
+const Login = ({ navigation }) => {
+  // Referencia al componente reCaptcha.
+  const recaptchaRef = useRef(null);
+  // Guarda el teléfono que escribe el usuario.
+  const [telefono, setTelefono] = useState("");
+  // Guarda el código SMS que escribe el usuario.
+  const [smsCodigo, setSmsCodigo] = useState("");
+  // Bandera para saber si se ha enviado el SMS.
+  const [codigoEnviado, setCodigoEnviado] = useState(false);
+  // Código de verificación al enviar un SMS.
+  const [verificationId, setVerificationId] = useState("");
+
+  // Observar el cambio de una variable de estado
+  useEffect(() => {
+    // Si aún no existe la verificación salir.
+    if (verificationId.length < 1) {
+      return;
+    }
+
+    // Establecemos que ya se mandó el SMS.
+    setCodigoEnviado(true);
+  }, [verificationId]);
+
+  // Response al evento con el teléfono del usuario.
+  const handlePhone = (text) => {
+    // Actualiza la variable de estado de la App.
+    setTelefono(text);
+  };
+
+  // Responde al evento del input con el código SMS.
+  const handleSmsCode = (text) => {
+    // Actualiza la variable de estado de la App.
+    setSmsCodigo(text);
+  };
+
+  // Response al botón "Enviar SMS".
+  const enviaCodigoSms = async () => {
+    // Agregamos el prefijo con códifo de país al teléfono tecleado.
+    const telefonoUsuario = '+52' + telefono;
+
+    // Creamos el componente.
+    const phoneProvider = new firebase.auth.PhoneAuthProvider();
+
+    // Enviamos el SMS.
+    const response = await phoneProvider.verifyPhoneNumber(
+      telefonoUsuario,
+      recaptchaRef.current
+    );
+
+    // Guardamos el código de verificación devuelto por firebase.
+    setVerificationId(response);
+  };
+
+  // Responde al segundo botón para verificar el código SMS.
+  const verificarCodigo = async () => {
+    // Creamos el objeto credencial con la verificación y el código SMS
+    // que teclea el usuario.
+    const credential = firebase.auth.PhoneAuthProvider.credential(
+      verificationId,
+      smsCodigo
+    );
+
+    try {
+      // Iniciamos sesión
+      const authResult = await firebase.auth().signInWithCredential(credential);
+
+      // Si todo sale bien, se manda a la pantalla principal.
+      navigation.navigate('Principal');
+    } catch (error) {
+      // Mostramos el error en la consola.
+      console.log(error);
+    }
+  };
+
+  const manejaCompartir = async () => {
+    Share.share({
+      message: 'Compartiendo el curso',
+      title: 'Curso 2021',
+      url: 'http://www.itchilpancingo.edu.mx/',
+    });
+  };
+
+  return (
+    <View style={styles.contenedor}>
+      <LogoItch />
+
+      <InputPhoneItch
+        valor={telefono}
+        alCambiarTexto={handlePhone}
+        textPorDefecto="Tu número a 10 dígitos"
+      />
+
+      <Button
+        title="Enviar SMS"
+        onPress={enviaCodigoSms}
+        disabled={!(telefono.length >= 10)}
+      />
+
+      <InputPhoneItch
+        max={6}
+        valor={smsCodigo}
+        alCambiarTexto={handleSmsCode}
+        textPorDefecto="Código SMS"
+        editable={codigoEnviado}
+      />
+      <Button
+        title="Confirmar código SMS"
+        onPress={verificarCodigo}
+        disabled={!codigoEnviado}
+      />
+
+      {/* <Button title="Compartir" onPress={manejaCompartir} /> */}
+
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaRef}
+        firebaseConfig={firebaseConfig}
+        attemptInvisibleVerification={true}
+      />
+    </View>
+  );
+};
+
+export default Login;
