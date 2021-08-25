d41792b4f1a2a2c37d5898c645d4404cbb373450
Luis Abarca <luisabarca@gmail.com>
2021-08-25 17:58:13 -0500

    Se mueve la configuración de firebase a un archivo externo

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCN_HQl9rLa2IsoJixR33F-5f0MqjzT9Cc",
+  authDomain: "cursotec2021.firebaseapp.com",
+  projectId: "cursotec2021",
+  storageBucket: "cursotec2021.appspot.com",
+  messagingSenderId: "431286485857",
+  appId: "1:431286485857:web:00e82aa6bb589d8f007b20",
+  measurementId: "G-LYG1T4VT6J",
+};
