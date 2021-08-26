f58f5ad1bff2b643dd59b3705679c7453d0c3306
Luis Abarca <luisabarca@gmail.com>
2021-08-26 18:24:19 -0500

    Agregadas constantes y funciones para guardar en el local storage si el usuario ya inicio sesión

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+
+export const SESSION_STATUS = 'sesion_status';
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SESSION_STATUS } from "./constants";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCN_HQl9rLa2IsoJixR33F-5f0MqjzT9Cc",
@@ -7,4 +9,45 @@ export const firebaseConfig = {
   messagingSenderId: "431286485857",
   appId: "1:431286485857:web:00e82aa6bb589d8f007b20",
   measurementId: "G-LYG1T4VT6J",
+};
+
+
+const guardarLocal  = async (clave, valor) => {
+  await AsyncStorage.setItem(clave, valor);
+};
+
+const leerLocal  = async (clave) => {
+  return await AsyncStorage.getItem(clave);
+};
+
+/**
+ * Establece un usuario con sesión activa.
+ */
+export const setUsuarioConSesionActiva = async () => {
+  await guardarLocal(SESSION_STATUS, 'si');
+};
+
+/**
+ * Establece un usuario con sesión activa.
+ */
+ export const setUsuarioSinSesionActiva = async () => {
+  await AsyncStorage.removeItem(SESSION_STATUS);
+};
+
+/**
+ * Obtiene el status de sesión de un usuario.
+ */
+ export const obtenUsuarioStatusUsuario = async () => {
+  return await leerLocal(SESSION_STATUS);
+};
+
+/**
+ * Verifica si el usuario tiene sesión activa.
+ * 
+ * @returns 
+ */
+export const esUsuarioConSesionActiva = async () => {
+  const valor = await leerLocal(SESSION_STATUS);
+
+  return valor == 'si';
 };
