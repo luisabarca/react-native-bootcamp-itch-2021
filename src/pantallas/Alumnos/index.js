1c96910f1a96df38825f37bb502209f48f72a4a7
Luis Abarca <luisabarca@gmail.com>
2021-08-26 18:32:42 -0500

    Mostramos una lista de alumnos en tiempo real utilizando el custom hook

--- src/pantallas/Alumnos/index.js
+++ src/pantallas/Alumnos/index.js
@@ -1,15 +1,74 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  ImageBackground,
+} from "react-native";
 
 import LogoItch from "../../componentes/Logo";
+import { useGetFirestoreAlumnos } from "../../utils/firestore";
 import styles from './styles';
 
 const Alumnos = () => {
+  // custom hook.
+  const items = useGetFirestoreAlumnos();
+
+  const renderAlumno = ({item}) => {
+    return (
+      <View style={{
+        flex: 1,
+        width: '100%',
+        padding: 10,
+      }}>
+        <ImageBackground
+          source={{
+            uri: item.foto,
+          }}
+          resizeMode="cover"
+          style={{
+            flex: 1,
+            height: 140,
+            width: '100%',
+            minWidth: '100%',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Text style={{
+            fontWeight: '500',
+            fontSize: 16,
+            color: 'white',
+            padding: 10,
+            backgroundColor: '#000000ee'
+          }}>
+            {item.nombre} {item.apellidos}
+          </Text>
+        </ImageBackground>
+      </View>
+    )
+  };
+
   return (
     <View style={styles.contenedor}>
       <LogoItch />
 
       <Text>Bienvenido alumno registrado</Text>
+
+      {
+        items.length > 0 && 
+        <FlatList 
+          data={items}
+          showsHorizontalScrollIndicator={false}
+          renderItem={renderAlumno}
+        />
+      }
+
+      {
+        items.length < 1 && 
+        <ActivityIndicator color="black" size="large" />
+      }
     </View>
   );
 };
